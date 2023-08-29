c9ed9d2bc1b254ced00becfb9469bd7f847301b4
mapmapbear <m.n.jhop@163.com>
2023-08-29 14:15:08 +0800

    fix sss shader webgl1 bug

--- res/src/3D/Advance/SeparableSSSRender/shader/SeparableSSS_Render.fs
+++ res/src/3D/Advance/SeparableSSSRender/shader/SeparableSSS_Render.fs
@@ -2,19 +2,19 @@
 #include "Color.glsl";
 varying vec2 v_Texcoord0;
 varying vec4 v_ScreenTexcoord;
-vec4 sampleMainTex(sampler2D tex, vec2 uv)
-{
-    vec4 mainSampler = texture2D(tex, uv);
-#ifdef Gamma_sssssDiffuseTexture
-    mainSampler = gammaToLinear(mainSampler);
-#endif // Gamma_u_MainTex
-    return mainSampler;
-}
+
 void main()
 {
 	vec4 color;
-	color =sampleMainTex(sssssDiffuseTexture,v_ScreenTexcoord.xy/v_ScreenTexcoord.w)+texture2D(sssssSpecularTexture, v_ScreenTexcoord.xy/v_ScreenTexcoord.w);
-
+	color = texture2D(sssssDiffuseTexture,v_ScreenTexcoord.xy/v_ScreenTexcoord.w);
+#ifdef Gamma_sssssDiffuseTexture
+    color = gammaToLinear(color);
+#endif // Gamma_Gamma_sssssDiffuseTexture
+    vec4 temp = texture2D(sssssSpecularTexture, v_ScreenTexcoord.xy/v_ScreenTexcoord.w);
+#ifdef Gamma_sssssDiffuseTexture
+    temp = gammaToLinear(temp);
+#endif // Gamma_sssssSpecularTexture
+    color += temp;
 	gl_FragColor = color;
 	gl_FragColor = outputTransform(gl_FragColor);
 }
